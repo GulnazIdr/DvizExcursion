1a5a3e53b43ee5b7d1e46a9a500bd5cad153ffbb
GulnazIdr <gulnazidr17@gmail.com>
2026-02-20 00:21:39 +0300

    Onboarding and login screens created

--- composeApp/src/androidMain/kotlin/org/gulnazidr/dviz_excursion/presentation/components/AnimatedBorderCard.kt
+++ composeApp/src/androidMain/kotlin/org/gulnazidr/dviz_excursion/presentation/components/AnimatedBorderCard.kt
@@ -10,12 +10,10 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -26,9 +24,7 @@ import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import org.gulnazidr.dviz_excursion.R
-
 
 @Composable
 fun AnimatedBorderCard(

d08b979547be09f5501c849b491155263a72a896
GulnazIdr <gulnazidr17@gmail.com>
2026-02-28 21:38:32 +0300

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/org/gulnazidr/dviz_excursion/presentation/theme/Colors.kt
@@ -0,0 +1,19 @@
+package org.gulnazidr.dviz_excursion.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFFC69FD4)
+val Background = Color.White
+val Error = Color.Red
+val Surface = Color.Red
+val OnSurface = Color(0xFFD3B8DC)
+
+val SuperDarkPurple = Color(0xFF280026)
+
+val LightPurple = Color(0xFFA460A1)
+val LighterPurple = Color(0xFFDFCCF1)
+val DescrGrey = Color(0xFF707B81)
+val LightGrey = Color(0xFFB6B6B6)
+
+val DarkPurple = Color(0xFF7B0377)
+val LighterOrange = Color(0xFFF7E1D3)
